660c005e6f157ad0547c1ca0529226555e54e709
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-19 20:50:22 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main()
+{
+
+char c;
+
+scanf("%c",&c);
+
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+
+printf("vowel",c);
+
+}
+
+else if((c>='a'&&c<='z')||(c>='A'&&c<='Z')){
+
+
+printf("Consonant",c);
+
+}
+
+else
+
+printf("Invalid");
+
+}
