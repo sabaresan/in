857f001b44399bf4738888dc98cd0f8ed5a4792f
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 22:02:34 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int n,fact=1;
+    
+scanf("%d",&n);
+    
+if(n<=0)
+     
+fact=1;
+     
+else
+     
+{
+         
+for(int i=1;i<=n;i++)
+         
+{
+             
+fact=fact*i;
+         
+}
+     
+}
+     
+printf("%d",fact);
+
+}
