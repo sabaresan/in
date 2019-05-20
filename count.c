7631009f77e67bbc84b7c1ca0e0d360783aff462
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 12:28:15 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int n;
+    
+int td=0;
+    
+scanf("%d",&n);
+    
+while(n!=0)
+ 
+{
+        
+n=n/10;
+        
+td++;
+    
+}
+    
+printf("%d",td);
+
+}
