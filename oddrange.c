7082f5795de6d23feecc3cbd0deed0415dc68f86
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 23:54:32 +0530

    Add files via upload

---
+++ oddrange.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main()
+{
+    
+
+
+int n,m,i,rem;
+
+
+scanf("%d%d",&n,&m);
+
+
+for(i=n+1;i<m;++i)
+
+
+{
+
+
+rem=i%2;
+
+
+if(rem==1)
+
+
+printf("%d ",i);
+
+
+}
+
+
+}
