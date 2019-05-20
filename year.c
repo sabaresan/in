ff2f0f9b283fe2ce4ea72382273b555f0cbc3551
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 11:29:14 +0530

    Add files via upload

---
+++ year.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int y;
+    
+scanf("%d",&y);
+    
+if(y%400==0)
+    
+printf("yes");
+    
+else if(y%100==0)
+    
+printf("no");
+    
+else if(y%4==0)
+    
+printf("yes");
+    
+else
+    
+printf("no");
+    
+
+}
