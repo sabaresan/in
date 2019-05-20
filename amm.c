0a3e6948360cc551a262d0fbfe1c5819f001076b
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 23:04:13 +0530

    Add files via upload

---
+++ amm.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+
+int main()
+{
+    
+
+int n,sum=0,r,temp;
+
+scanf("%d",&n);
+
+temp=n;    
+
+while(n>0)
+{
+ 
+r=n%10;
+ 
+sum=sum+(r*r*r);
+ 
+n=n/10;
+
+}
+
+if(temp==sum)
+
+printf("yes");
+
+else
+
+printf("no");
+
+
+}
