60e7fc1760708f1a20f2936617a11464865f670d
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 11:12:07 +0530

    Add files via upload

---
+++ large.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+   
+int n,m,o;
+   
+scanf("%d%d%d",&n,&m,&o);
+   
+if(n>=m&&n>=o)
+   
+printf("%d",n);
+   
+else if(m>=n&&m>=o)
+   
+printf("%d",m);
+   
+else
+   
+printf("%d",o);
+
+}
