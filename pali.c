25ff282d68856b6184d642cf1b51527220f0544a
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 13:48:02 +0530

    Add files via upload

---
+++ pali.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int n,m,rev=0;
+    
+scanf("%d",&n);
+    
+m=n;
+    
+while(m!=0)
+   
+{
+        
+rev=rev*10;
+        
+rev=rev+m%10;
+        
+m=m/10;
+    
+}
+    
+if(n==rev)
+    
+printf("yes");
+    
+else
+    
+printf("no");
+    
+
+}
