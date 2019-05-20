a517880b2d5032e66f715da03846c170a96c7b96
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 13:41:18 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int n,m,res=1;
+    
+scanf("%d%d",&n,&m);
+    
+while(m!=0)
+    
+{
+        
+res*=n;
+        
+--m;
+    
+}
+    
+printf("%d",res);
+
+}
