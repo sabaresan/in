9647642aeb34143daf668ddfc37a187a36ff7879
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 23:49:49 +0530

    Add files via upload

---
+++ primerange.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int main()
+{
+   
+int n,m,i,j,flag;
+   
+scanf("%d%d",&n,&m);
+   
+       
+for(i=n+1;i<m;++i)
+       
+{
+           
+flag=0;
+           
+for(j=2;j<=i/2;++j)
+           
+{
+               
+if(i%j==0)
+               
+{
+                   
+flag=1;
+                   
+break;
+               
+}
+           
+}
+           
+if(flag==0)
+           
+printf("%d ",i);
+       
+}
+ 
+
+}
