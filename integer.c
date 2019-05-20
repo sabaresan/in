614aeb34c71b8ef72fd3b4006711cbd4ca857e07
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-20 19:03:22 +0530

    Add files via upload

---
+++ integer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int n,k,arr[100],count=0;
+    
+scanf("%d%d",&n,&k);
+    
+for(int i=0;i<n;i++)
+    
+{
+        
+scanf("%d",&arr[i]);
+        
+    
+}
+    
+for(int i=0;i<k;i++)
+    
+{
+        
+count=count+arr[i];
+    
+}
+    
+printf("%d",count);
+
+}
