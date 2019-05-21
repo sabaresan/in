b58fc3ead7257c681a12debea93385435c39b80b
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-21 13:44:53 +0530

    Add files via upload

---
+++ medarr.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define N 5
+
+int main()
+{
+    
+int n,i,j;
+    
+int median,a[N],temp;
+    
+scanf("%d",&n);
+    
+for(i=0;i<n;i++)
+    
+{
+        
+scanf("%d",&a[i]);
+    
+}
+    
+temp=0;
+        
+for(i=0;i<n-1;i++)
+        
+{
+            
+for(j=0;j<(n-i-1);j++)
+            
+{
+                
+if(a[j]<a[j+1])
+                
+{
+                    
+temp=a[j];
+                    
+a[j]=a[j+1];
+                    
+a[j+1]=temp;
+                
+}
+            
+}
+        
+}
+        
+if(n%2==0)
+        
+median=(a[(n/2)-1]+a[(n/2)])/2;
+        
+else
+        
+median=a[(n/2)];
+        
+printf("%d ",median);
+
+}
