633d4867be646c37a835d79d21cb9164e2168d10
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-21 13:06:29 +0530

    Add files via upload

---
+++ minarr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+int arr[50],n,i,min;
+    
+scanf("%d",&n);
+    
+for(i=0;i<n;i++)
+        
+scanf("%d",&arr[i]);
+        
+min=arr[0];
+        
+for(i=1;i<n;i++)
+        
+{
+            
+if(min>arr[i])
+            
+min=arr[i];
+        
+}
+        
+printf("%d",min);
+
+}
