e55ff503444274f52ee9a30a2349665d5eab789b
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-21 21:02:09 +0530

    Add files via upload

---
+++ fibo.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    
+int n,f=0,s=1,next,i;
+    
+scanf("%d",&n);
+    
+for(i=1;i<=n;i++)
+    
+{
+        
+if(i<=1)
+            
+next=i;
+            
+else
+            
+{
+                
+next=f+s;
+                
+f=s;
+                
+s=next;
+            
+}
+        
+printf("%d ",next);
+    
+}
+
+}
