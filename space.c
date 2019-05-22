461ee793b2bf6f3dffc90ab3342083d5ab304f28
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-22 11:30:40 +0530

    Add files via upload

---
+++ space.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+{
+   
+char str[50];
+   
+int i,count=0;
+   
+gets(str);
+   
+for(i=0;str[i];i++)
+   
+if(str[i]==' ')
+   
+count++;
+   
+printf("%d",count);
+
+}
