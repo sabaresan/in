be56e1ab8df41e9fd1adebed4f68dad0ab4b0023
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 12:15:48 +0530

    Add files via upload

---
+++ ap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+    
+int a,n,d,i,s=0,tn;
+    
+scanf("%d%d%d",&a,&d,&n);
+    
+s=(n*(2*a+(n-1)*d))/2;
+    
+tn=a+(n-1)*d;
+    
+for(i=a;i<=tn;i=i+d)
+    
+{ 
+        
+printf("%d",s);
+    
+}
+
+}
