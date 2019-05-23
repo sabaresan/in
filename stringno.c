1123ada62494402a02709b7d47541d51d9eef81c
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 13:28:47 +0530

    Add files via upload

---
+++ stringno.c
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+class GFG 
+{
+	
+public static void main (String[] args) 
+	
+{
+	    
+String s;
+	    
+Scanner sc=new Scanner(System.in);
+	    
+s=sc.nextLine();
+	    
+try
+	    
+{
+	        
+	        
+Integer.parseInt(s);
+	        
+System.out.println("yes");
+	        
+	   
+} 
+	    
+catch(NumberFormatException e)
+	    
+{
+	        
+System.out.println("no");
+	        
+	    
+}
+	    
+	
+}    
+	  
+
+}
